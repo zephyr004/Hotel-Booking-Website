805e727ba8b48fac04bb99b10c0c6967103a1b1c
zephyr004 <jigyasa042003@gmail.com>
2025-06-05 23:36:16 +0530

    new changes

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -14,8 +14,8 @@ import { Link } from 'react-router-dom';
           <div>Any week </div>
           <div className='border-l border-gray-300'></div>
           <div>Add guests</div>
-          <button className='bg-primary text-black p-1 rounded-full'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+          <button style={{backgroundColor: '#F5385D'}} className='bg-[#F5385D] text-black p-1 rounded-full'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-5">
               <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
           </button>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
-
