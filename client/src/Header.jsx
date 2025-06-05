1fdaac20fe5d3e6d48a56f3b583d1162b75fe2a8
zephyr004 <jigyasa042003@gmail.com>
2025-06-05 22:08:37 +0530

    initial commit

---
+++ client/src/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+ export default function Header(){
+    return(
+      <header className='p-4 flex justify-between'>
+        <a href="" className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 -rotate-90">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+          </svg>
+          <span className="font-bold text-xl">  airbnb</span>
+        </a>
+        <div className='flex gap-2 border border-gray-300 rounded-full py-1 px-4 shadow-md shadow-gray-300'>
+          <div>Anywhere</div>
+          <div className='border-l border-gray-300'></div>
+          <div>Any week </div>
+          <div className='border-l border-gray-300'></div>
+          <div>Add guests</div>
+          <button className='bg-primary text-black p-1 rounded-full'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+        </div>
+        <Link to={'/login'} className='flex items- center gap-2 border border-gray-300 rounded-full py-2 px-4'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+          <div className='bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+              <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+            </svg>
+          </div>
+        </Link>
+      </header>
+    );
+}
