25a24e254980ea9a09cec89a9f5a9941588f290c
zephyr004 <jigyasa042003@gmail.com>
2025-06-05 23:44:24 +0530

    new commit

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -20,7 +20,7 @@ import { Link } from 'react-router-dom';
             </svg>
           </button>
         </div>
-        <Link to={'/login'} className='flex items- center gap-2 border border-gray-300 rounded-full py-2 px-4'>
+        <Link to={'/login'} className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
